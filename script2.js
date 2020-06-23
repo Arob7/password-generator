8fdd5ea2c76c06cda91e6aac9f89d77380cc7a4b
Arob7 <66214652+Arob7@users.noreply.github.com>
2020-06-22 21:50:43 -0400

    added styles.css page

---
+++ script2.js
@@ -0,0 +1,16 @@
+//get password length
+//get uppercase, lowercase, number, special
+
+//validate input
+// if (choicesMade){
+//     generatePassword
+// } else{
+//     console.log("Please enter characters")
+// }
+
+//generate password  
+// get characters
+//randomly select characters to make password of x length
+
+
+//display password
