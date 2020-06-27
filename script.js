ebbf29fe60393e79bed3284eb204b82cdcae5930
Arob7 <66214652+Arob7@users.noreply.github.com>
2020-06-27 00:39:17 -0400

    added responsive slider

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 //generate password
 function generate() {
-  //get password length
-  let choice = document.getElementById("slider").value;
-
   //get uppercase, lowercase, number, special
+  let choice = document.getElementById("slider").value;
   let values =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
 
+  // get characters
   let password = "";
 
   //validate input
@@ -16,15 +15,25 @@ function generate() {
   //     console.log("Please enter characters")
   // }
 
-  // get characters
   for (var i = 0; i <= choice; i++) {
+    //randomly select characters to make password of x length
     password =
       password +
       values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
   }
 
-  //randomly select characters to make password of x length
-
   //display password
   document.getElementById("display").value = password;
+
+  //get password length
+  document.getElementById("length").innerHTML = "Length: 25";
+
+  document.getElementById("slider").oninput = function () {
+    if (document.getElementById("slider").value > 0) {
+      document.getElementById("length").innerHTML =
+        "Length: " + document.getElementById("slider").value;
+    } else {
+      document.getElementById("length").innerHTML = "Length: 1";
+    }
+  };
 }
