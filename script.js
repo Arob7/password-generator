3956f7018fa0423ae06fae550301e38769e43b25
Arob7 <66214652+Arob7@users.noreply.github.com>
2020-06-26 23:55:58 -0400

    added JS functions and generates random characters

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
-//get password length
-//get uppercase, lowercase, number, special
-
-//validate input
-// if (choicesMade){
-//     generatePassword
-// } else{
-//     console.log("Please enter characters")
-// }
-
 //generate password
-function generate() {}
+function generate() {
+  //get password length
+  let choice = document.getElementById("slider").value;
 
-// get characters
-//randomly select characters to make password of x length
+  //get uppercase, lowercase, number, special
+  let values =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
 
-//display password
+  let password = "";
+
+  //validate input
+  // if (choicesMade){
+  //     generatePassword
+  // } else{
+  //     console.log("Please enter characters")
+  // }
+
+  // get characters
+  for (var i = 0; i <= choice; i++) {
+    password =
+      password +
+      values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+  }
+
+  //randomly select characters to make password of x length
+
+  //display password
+  document.getElementById("display").value = password;
+}
