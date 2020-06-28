4fcaa876a4d369ba13d817453e32dc94aed56967
Arob7 <66214652+Arob7@users.noreply.github.com>
2020-06-27 22:23:53 -0400

    added check boxes, and button generates random password, slider captures length of password

--- script.js
+++ script.js
@@ -1,39 +1,62 @@
-//generate password
-function generate() {
-  //get uppercase, lowercase, number, special
-  let choice = document.getElementById("slider").value;
-  let values =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
+// console.log(choices);
 
-  // get characters
+// var complexity = document.getElementById("slider").value;
+
+// document.getElementById("slider").oninput = function(){
+
+//   // get slider value to display as password "length"
+//   document.getElementById("length").innerHTML = "Length: " + choice;
+
+// }
+
+//get uppercase, lowercase, number, special
+function gen() {
+  let lowercase = "abcdefghijklmnopqrstuvwxyz";
+  let uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let numbers = "1234567890";
+  let specialChar = "!@#$%^&*()_+";
+  let val = lowercase + uppercase + numbers + specialChar;
+
   let password = "";
-
-  //validate input
-  // if (choicesMade){
-  //     generatePassword
-  // } else{
-  //     console.log("Please enter characters")
-  // }
-
-  for (var i = 0; i <= choice; i++) {
+  var choice = document.getElementById("slider").value;
+  for (var i = 0; i < choice; i++) {
     //randomly select characters to make password of x length
     password =
       password +
-      values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+      val.charAt(Math.floor(Math.random() * Math.floor(val.length - 1)));
   }
 
-  //display password
+  // //display password
   document.getElementById("display").value = password;
-
-  //get password length
-  document.getElementById("length").innerHTML = "Length: 25";
-
-  document.getElementById("slider").oninput = function () {
-    if (document.getElementById("slider").value > 0) {
-      document.getElementById("length").innerHTML =
-        "Length: " + document.getElementById("slider").value;
-    } else {
-      document.getElementById("length").innerHTML = "Length: 1";
-    }
-  };
 }
+
+// //get password length
+document.getElementById("length").innerHTML = "Length: 12";
+
+document.getElementById("slider").oninput = function () {
+  // if (document.getElementById("slider").value > 0) {
+
+  document.getElementById("length").innerHTML =
+    "Length: " + document.getElementById("slider").value;
+
+  // } else {
+  //   document.getElementById("length").innerHTML = "Length: 8";
+  // }
+};
+
+//get password length
+//get uppercase, lowercase, number, special
+
+//validate input
+// if (choicesMade){
+//     generatePassword
+// } else{
+//     console.log("Please enter characters")
+// }
+
+//generate password
+
+// get characters
+//randomly select characters to make password of x length
+
+//display password
