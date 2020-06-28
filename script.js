d21c2b953245e3c579b76a0a6d4352e1df4af020
Arob7 <66214652+Arob7@users.noreply.github.com>
2020-06-28 14:05:35 -0400

    centered content on page

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-// console.log(choices);
-
-// var complexity = document.getElementById("slider").value;
-
-// document.getElementById("slider").oninput = function(){
-
-//   // get slider value to display as password "length"
-//   document.getElementById("length").innerHTML = "Length: " + choice;
-
-// }
-
 //get uppercase, lowercase, number, special
 function gen() {
   let lowercase = "abcdefghijklmnopqrstuvwxyz";
@@ -43,20 +32,3 @@ document.getElementById("slider").oninput = function () {
   //   document.getElementById("length").innerHTML = "Length: 8";
   // }
 };
-
-//get password length
-//get uppercase, lowercase, number, special
-
-//validate input
-// if (choicesMade){
-//     generatePassword
-// } else{
-//     console.log("Please enter characters")
-// }
-
-//generate password
-
-// get characters
-//randomly select characters to make password of x length
-
-//display password
