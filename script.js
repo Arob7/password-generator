37ba89faf8f18ae45cb98651fc9aadee309347a0
Arob7 <66214652+Arob7@users.noreply.github.com>
2020-06-28 17:42:30 -0400

    changed checkboxes to all checked

--- script.js
+++ script.js
@@ -23,17 +23,20 @@ function gen() {
 document.getElementById("length").innerHTML = "Length: 12";
 
 document.getElementById("slider").oninput = function () {
-  // if (document.getElementById("slider").value > 0) {
-
   document.getElementById("length").innerHTML =
     "Length: " + document.getElementById("slider").value;
-
-  // } else {
-  //   document.getElementById("length").innerHTML = "Length: 8";
-  // }
 };
 
 // NEED CONDITION STATEMENT FOR CHECKBOXES
+function box() {
+  var checkbox = document.getElementById(uppercase);
+
+  if (checkbox == true) {
+    console.log("pulls uppercase");
+  } else {
+    console.log("excludes uppercase");
+  }
+}
 
 // function box() {
 //   allChecked (document.querySelectorAll(#uppercase,
